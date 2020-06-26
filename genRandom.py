215064ebb4c69f8b950458dede349eec3e340fba
CHEN WEI-TING <williamchen180@gmail.com>
2020-06-26 10:52:38 +0800

    backup

---
+++ genRandom.py
@@ -0,0 +1,11 @@
+
+
+
+import random
+
+with open('random.txt', 'w') as f:
+    for i in range(62, 2792, 10):
+        r = random.sample(range(10), 10)
+        for j in range(0, 10):
+            f.write(str(r[j]) + '\n')
+
